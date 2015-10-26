5394c31ed32fec8b4a0736725c147d950918d9e6
Alex <bushin-sasha@mail.ru>
2015-10-26 20:12:07 +0300

    самат быдло

---
+++ Step_v0/Step_v0/Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace Step_v0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textNomer = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Receive_data1 = new System.Windows.Forms.Button();
+            this.Vivod_data = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Receive_data2 = new System.Windows.Forms.Button();
+            this.Receive_data3 = new System.Windows.Forms.Button();
+            this.Clean = new System.Windows.Forms.Button();
+            this.Count = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(198, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textNomer
+            // 
+            this.textNomer.Enabled = false;
+            this.textNomer.Location = new System.Drawing.Point(704, 155);
+            this.textNomer.Name = "textNomer";
+            this.textNomer.Size = new System.Drawing.Size(100, 20);
+            this.textNomer.TabIndex = 1;
+            this.textNomer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(432, 215);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(432, 155);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(198, 272);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(198, 215);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(195, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Поиск по пассажиру";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(82, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(82, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Имя";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(82, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Отчество";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(429, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Поиск по маршруту";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(701, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Поиск по номеру поезда";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(352, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Отправление";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(352, 222);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Прибытие";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(641, 162);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Номер";
+            // 
+            // Receive_data1
+            // 
+            this.Receive_data1.Location = new System.Drawing.Point(388, 435);
+            this.Receive_data1.Name = "Receive_data1";
+            this.Receive_data1.Size = new System.Drawing.Size(134, 40);
+            this.Receive_data1.TabIndex = 15;
+            this.Receive_data1.Text = "Принять данные";
+            this.Receive_data1.UseVisualStyleBackColor = true;
+            this.Receive_data1.Visible = false;
+            this.Receive_data1.Click += new System.EventHandler(this.Receive_data1_Click);
+            // 
+            // Vivod_data
+            // 
+            this.Vivod_data.FormattingEnabled = true;
+            this.Vivod_data.Location = new System.Drawing.Point(627, 215);
+            this.Vivod_data.Name = "Vivod_data";
+            this.Vivod_data.Size = new System.Drawing.Size(383, 407);
+            this.Vivod_data.TabIndex = 16;
+            this.Vivod_data.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(130, 17);
+            this.radioButton1.TabIndex = 17;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Поиск по пассажиру";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(124, 17);
+            this.radioButton2.TabIndex = 18;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Поиск по маршруту";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(151, 17);
+            this.radioButton3.TabIndex = 19;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Поиск по номеру поезда";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(21, 360);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(215, 100);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор поиска";
+            // 
+            // Receive_data2
+            // 
+            this.Receive_data2.Location = new System.Drawing.Point(388, 436);
+            this.Receive_data2.Name = "Receive_data2";
+            this.Receive_data2.Size = new System.Drawing.Size(134, 39);
+            this.Receive_data2.TabIndex = 21;
+            this.Receive_data2.Text = "Принять данные";
+            this.Receive_data2.UseVisualStyleBackColor = true;
+            this.Receive_data2.Visible = false;
+            this.Receive_data2.Click += new System.EventHandler(this.Receive_data2_Click);
+            // 
+            // Receive_data3
+            // 
+            this.Receive_data3.Location = new System.Drawing.Point(388, 435);
+            this.Receive_data3.Name = "Receive_data3";
+            this.Receive_data3.Size = new System.Drawing.Size(134, 39);
+            this.Receive_data3.TabIndex = 22;
+            this.Receive_data3.Text = "Принять данные";
+            this.Receive_data3.UseVisualStyleBackColor = true;
+            this.Receive_data3.Visible = false;
+            this.Receive_data3.Click += new System.EventHandler(this.Receive_data3_Click);
+            // 
+            // Clean
+            // 
+            this.Clean.Location = new System.Drawing.Point(388, 505);
+            this.Clean.Name = "Clean";
+            this.Clean.Size = new System.Drawing.Size(134, 42);
+            this.Clean.TabIndex = 23;
+            this.Clean.Text = "Отчистить";
+            this.Clean.UseVisualStyleBackColor = true;
+            this.Clean.Click += new System.EventHandler(this.Clean_Click);
+            // 
+            // Count
+            // 
+            this.Count.Location = new System.Drawing.Point(388, 569);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(134, 40);
+            this.Count.TabIndex = 24;
+            this.Count.Text = "Рассчет";
+            this.Count.UseVisualStyleBackColor = true;
+            this.Count.Visible = false;
+            this.Count.Click += new System.EventHandler(this.Count_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1090, 610);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.Clean);
+            this.Controls.Add(this.Receive_data3);
+            this.Controls.Add(this.Receive_data2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Vivod_data);
+            this.Controls.Add(this.Receive_data1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textNomer);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textNomer;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button Receive_data1;
+        private System.Windows.Forms.ListBox Vivod_data;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Receive_data2;
+        private System.Windows.Forms.Button Receive_data3;
+        private System.Windows.Forms.Button Clean;
+        private System.Windows.Forms.Button Count;
+    }
+}
+
---
+++ Step_v0/Step_v0/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Step_v0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] info;
+        string[] route;
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                textBox1.Enabled = true;
+                textBox6.Enabled = true;
+                textBox5.Enabled = true;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textNomer.Enabled = false;
+                Receive_data2.Visible = true;
+                Receive_data1.Visible = false;
+                Receive_data3.Visible = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+                textBox1.Enabled = false;
+                textBox6.Enabled = false;
+                textBox5.Enabled = false;
+                textNomer.Enabled = false;
+                Receive_data3.Visible = true;
+                Receive_data2.Visible = false;
+                Receive_data1.Visible = false;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+                textNomer.Enabled = true;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox1.Enabled = false;
+                textBox6.Enabled = false;
+                textBox5.Enabled = false;
+                Receive_data1.Visible = true;
+                Receive_data2.Visible = false;
+                Receive_data3.Visible = false;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if ((Char.IsDigit(e.KeyChar)) || (Char.IsControl(e.KeyChar)))
+                e.Handled = false;
+        }
+
+        private void Receive_data1_Click(object sender, EventArgs e)
+        {
+            Train train = new Train();
+            List<string> collection = new List<string>();
+            StreamReader fs1 = new StreamReader("baseV2.txt");
+            while (true) 
+            {
+                string s = fs1.ReadLine();
+
+                if (s != null)
+                {
+                    collection.Add(s);
+                }
+                else
+                    break;
+            }
+            string str = textNomer.Text;
+            int k = 0;
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (collection[i].Contains(str))
+                {
+                    info = collection[i].Split(' ');
+                    route = info[4].Split('-');
+                    textBox4.Text = route[0];
+                    textBox3.Text = route[1];
+                    k++;
+                    Vivod_data.Items.Add(collection[i]);
+                    Count.Visible = true;
+                }
+            }
+
+            if (k == 0)
+            {
+                MessageBox.Show("Не найденно совпадений");
+                Vivod_data.Items.Add("Совпадений не найдено");
+            }
+                // string s = Properties.Resources.Base;
+                Vivod_data.Visible = true;
+        }
+
+        private void Receive_data2_Click(object sender, EventArgs e)
+        {
+            Passanger passanger = new Passanger();
+            List<string> collection = new List<string>();
+            StreamReader fs2 = new StreamReader("baseV3.txt");
+            while (true)
+            {
+                string s = fs2.ReadLine();
+
+                if (s != null)
+                {
+                    collection.Add(s);
+                }
+                else
+                    break;
+            }
+            string str1 = textBox1.Text;
+            string str2 = textBox6.Text;
+            string str3 = textBox5.Text;
+            int k = 0;
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if ((collection[i].Contains(str1)) && (collection[i].Contains(str2)) && (collection[i].Contains(str3)))
+                {
+                    string[] info = collection[i].Split(' ');
+                    passanger.Surname = info[0];
+                    passanger.Name = info[1];
+                    passanger.Secondname = info[2];
+                    k++;
+                    Vivod_data.Items.Add(collection[i]);
+                }
+            }
+
+            if (k == 0)
+            {
+                MessageBox.Show("Таких людей нет");
+                Vivod_data.Items.Add("Таких людей нет");
+            }
+            // string s = Properties.Resources.Base;
+            Vivod_data.Visible = true;
+        }
+
+        private void Receive_data3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clean_Click(object sender, EventArgs e)
+        {
+            Vivod_data.Items.Clear();
+        }
+
+        private void Count_Click(object sender, EventArgs e)
+        {
+            List<string> collection = new List<string>();
+            StreamReader fs3 = new StreamReader("baseV4.txt");
+            Train train = new Train();
+            while (true)
+            {
+                string s = fs3.ReadLine();
+
+                if (s != null)
+                {
+                    collection.Add(s);
+                }
+                else
+                    break;
+            }
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if ((collection[i].Contains(route[0])) && (collection[i].Contains(route[1])))
+                {
+                   string[] route1 = collection[i].Split(' ');
+                     train.Distance = int.Parse(route1[2]);
+                }
+            }  
+            train.Nomer = info[0];
+            train.Type = info[1];
+            train.Speed = int.Parse(info[2]);
+            Vivod_data.Items.Add("Поезд будет в пути:");
+            Vivod_data.Items.Add(Train.AnalizInfo(train.Distance, train.Speed) + " часов");
+        }
+    }
+
+    public class Train {
+        public string Nomer, Type;
+        public int Speed, Distance;
+
+        public static int AnalizInfo(int distance, int speed)
+        {
+            int time = distance / speed;
+            return time;
+        }
+    }
+
+    public class Passanger {
+        public string Surname, Name, Secondname;
+
+        public void AnalizInfo(string surname, string name, string secondname)
+        {
+            // Analiz
+        }
+    }
+
+}
